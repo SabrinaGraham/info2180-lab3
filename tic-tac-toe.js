0ccd21f477f7d9e451c5a5a39725b31d54a3a00e
Sabrina Graham <techiepotato3004@gmail.com>
2021-10-09 18:44:18 -0400

    Exercise4- Check for winner and update status

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,5 +1,12 @@
 document.addEventListener('DOMContentLoaded',(event)=>{
 
+    //variables for deciding who the winner is
+
+    let match=0; //the number of selected boxes that corresponds to those in the possible_wins array
+    let x_selected=[]; //array to store x moves
+    let o_selected=[]; //array to store o moves
+
+
     //get div elements in the board div
     let getboard=document.getElementById('board').children;
 
@@ -18,10 +25,24 @@ document.addEventListener('DOMContentLoaded',(event)=>{
 
     showGameBoard(); //invoke showGameBoard function
     
-    let x_selected=[]; //array to store x moves
-    let o_selected=[]; //array to store o moves
+    
+    
+    //Announce Winner Function
+    function winnermsg(who)
+    {
+        let winmsg=document.getElementById('status');
+        winmsg.classList.add('you-won');
+        switch(who)
+        {
+            case 'x': winmsg.textContent='Congratulations! X is the Winner!'; break;
+            case 'o': winmsg.textContent='Congratulations! O is the Winner!'; break;
+            case 'd': winmsg.textContent='Aw! That is a Draw'; break;
+        }
+        match=-1; // when winner is announced, set match to -1 so that no other cell can be selected.
+    }
 
-    //x-o alternation
+
+    //GameMaster Function
     function xo(ele,clicked,ind)
     {
         if (clicked==true)
@@ -29,47 +50,96 @@ document.addEventListener('DOMContentLoaded',(event)=>{
                 ele.classList.add('X'); 
                 ele.textContent='X';
                 x_selected.push(ind);
+                if (x_selected.length>=3)   //if player X has played atleast 3 times already
+                {
+                    match=0
+                    findWinner('x',x_selected); //check if those 3 X's are in a row
+                    if (match==3)               //if there are 3 x's in a row
+                    {
+                        winnermsg('x');         //display winner message
+                    }  
+                }
             }
         else
             {
                 ele.classList.add('O');
                 ele.textContent='O';
                 o_selected.push(ind);
+                if (o_selected.length>=3)   //if player O has played atleast 3 times already
+                {
+                    match=0;
+                    findWinner('o',o_selected); //check if those 3 O's are in a row
+                    if (match==3)               //if there are 3 O's in a row
+                    {
+                        winnermsg('o');         //display winner message
+                    }
+                }
             }
+        
         return !clicked;
     }
 
     
-    let cell=[...document.getElementsByClassName('square')];
-    let alt=true;
+    let cell=[...document.getElementsByClassName('square')];//convert the HTML collection of div elements to an array
+    let alt=true; //variable used to alternate b/w player X and O
     
     
+    //Applying Interactive features to the cells in the Game board
     cell.forEach((elem,index)=>
     {
         //show x or o after click
         elem.addEventListener('click', e=>
             {
                 e.preventDefault();
-                alt=xo(elem,alt,index);
-                //console.log('o:'+o_selected+'x:'+x_selected);
-                //elem.textContent='X';
+                if(match!==-1) // If match is -1 then no other cell can be selected to show either x or o
+                    {
+                        alt=xo(elem,alt,index); //GameMaster decides what to do after cell is clicked
+                    }
+                if (x_selected.length+o_selected.length==9) //if all the cells have been selcted and there is no winner then its a draw
+                    {
+                        console.log("Game Draw!");
+                        winnermsg('d');
+                    }
             })
-
+        
         //Highlight box when mouse hovers over
         elem.addEventListener('mouseover', e=>
             {
                 e.preventDefault();
-                e.target.classList.add('hover');
+                e.target.classList.add('hover'); //cell highlighted when mouse hovers
             });
         elem.addEventListener('mouseout', e=>
             {
                 e.preventDefault();
-                e.target.classList.remove('hover');
+                e.target.classList.remove('hover'); //cell dehighlighted when mouse moves to another location
             });
    });
    
-
     
+    let possible_wins=[[0,1,2],[0,3,6],[0,4,8],[2,4,6],[2,5,8],[6,7,8],[3,4,5],[1,4,7]]; //possible ways to win
     
+    //Function to find which player made moves that match any of the possible ways to win in the possible_wins array
+    function findWinner(player,candidate)
+        {
+            match=0;
+            possible_wins.some((el,ind)=>
+            {
+                el.forEach((num,pos)=>
+                {
+                    if((candidate.includes(num))===true) //if boxes selected by player includes any of the boxes needed to win 
+                    {
+                        match++;                        //add 1 to match...if match increments to 3 then the player has won
+                    }
+                });
+                if (match<3)
+                {
+                    match=0; // reset match for next array in possible_wins
+                }
+                else if (match==3)
+                {
+                    return true; //escape loop and keep state of variable match
+                }
+            });
+        }
 
 });
